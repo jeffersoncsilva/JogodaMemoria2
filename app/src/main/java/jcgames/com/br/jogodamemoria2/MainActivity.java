67d7c974b21751c66a0d33e4a283317010049fe5
Jefferson Carvalho <jefferson.c.silva@outlook.com>
2016-04-08 17:14:47 -0300

    Inicio da integração com o Facebook SDK

--- app/src/main/java/jcgames/com/br/jogodamemoria2/MainActivity.java
+++ app/src/main/java/jcgames/com/br/jogodamemoria2/MainActivity.java
@@ -3,10 +3,15 @@ package jcgames.com.br.jogodamemoria2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.MainThread;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
 
+
+/*
+* Site do tutorial do facebook SDK:
+* https://developers.facebook.com/docs/facebook-login/android
+* */
 
 public class MainActivity extends Activity {
     Button facil;
